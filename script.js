b6df7238fc2377ff590c9424b34dff280bb6e981
ggonzalesu <101446821+ggonzalesu@users.noreply.github.com>
2023-05-26 00:51:03 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,3 @@ var copy = document.querySelector(".logos__slide").cloneNode(true);
 document.querySelector('.logos').appendChild(copy);
 
 
-const shareBtn = document.getElementById('shareBtn')
-
-shareBtn.addEventListener('click', event => {
-    if (navigator.share) {
-        navigator.share({
-            text: 'please read this great article: ',
-            url: 'https://www.google.com/'
-        }).then(() => {
-            console.log('thanks for sharing');
-        })
-
-            .catch(err) => console.error(err);
-    } else {
-        alert("El navegador no soporta function, hacerlo manual")
-    }
-});
--- script_product.js
+++ script_product.js
@@ -3,8 +3,8 @@ const shareBtn = document.getElementById('shareBtn');
 shareBtn.addEventListener('click', event => {
     if (navigator.share) {
         navigator.share({
-            title: 'Please read this great article:',
-            url: 'https://www.google.com/'
+            title: '',
+            url: 'https://www.remotosgsm.net/'
         }).then(() => {
             console.log('Thanks for sharing');
         }).catch(console.error);
